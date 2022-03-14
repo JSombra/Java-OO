09a43ae278fe2a4dd6e45519e190396ab8e918ac
Jefferson Sombra <soombra73@gmail.com>
2022-03-13 22:19:34 -0300

    ajuster arquivos

--- src/TestarValores.java
+++ src/TestarValores.java
@@ -4,6 +4,10 @@ public class TestarValores {
         Conta conta2 = new Conta(1337, 26225);
         Conta conta3 = new Conta(1337, 14225);
         Conta conta4 = new Conta(1337, 64225);
+
+        System.out.println(conta1.getAgencia());
+        System.out.println(conta2.getAgencia());
+        System.out.println(conta3.getAgencia());
         System.out.println(conta4.getAgencia());
         System.out.println(conta4.getTotal());
     }
